0a2514f081cb877715c4af9531fc9e759eae4fcc
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-29 08:35:17 +0700

    update card

--- src/data/expense.jsx
+++ src/data/expense.jsx
@@ -1,4 +1,5 @@
-export const expensesBreakdowns = [
+// expenses.js
+const expensesBreakdowns = [
     {
       id: 1,
       category: "Housing",
@@ -49,49 +50,51 @@ export const expensesBreakdowns = [
     },
   ];
   
-  export const expensesStatistics = [
-    {
-      id: 1,
-      date: "17 Sun",
-      amountThisWeek: 250000,
-      amountLastWeek: 50000,
-    },
-    {
-      id: 2,
-      date: "18 Mon",
-      amountThisWeek: 50000,
-      amountLastWeek: 10000,
-    },
-    {
-      id: 3,
-      date: "19 Tue",
-      amountThisWeek: 10000,
-      amountLastWeek: 50000,
-    },
-    {
-      id: 4,
-      date: "20 Wed",
-      amountThisWeek: 50000,
-      amountLastWeek: 50000,
-    },
-    {
-      id: 5,
-      date: "21 Thu",
-      amountThisWeek: 50000,
-      amountLastWeek: 10000,
-    },
-    {
-      id: 6,
-      date: "22 Fri",
-      amountThisWeek: 250000,
-      amountLastWeek: 10000,
-    },
-    {
-      id: 7,
-      date: "23 Sat",
-      amountThisWeek: 50000,
-      amountLastWeek: 10000,
-    },
-  ];
-  
+  // Only default export
   export default expensesBreakdowns;
+  
+    export const expensesStatistics = [
+      {
+        id: 1,
+        date: "17 Sun",
+        amountThisWeek: 250000,
+        amountLastWeek: 50000,
+      },
+      {
+        id: 2,
+        date: "18 Mon",
+        amountThisWeek: 50000,
+        amountLastWeek: 10000,
+      },
+      {
+        id: 3,
+        date: "19 Tue",
+        amountThisWeek: 10000,
+        amountLastWeek: 50000,
+      },
+      {
+        id: 4,
+        date: "20 Wed",
+        amountThisWeek: 50000,
+        amountLastWeek: 50000,
+      },
+      {
+        id: 5,
+        date: "21 Thu",
+        amountThisWeek: 50000,
+        amountLastWeek: 10000,
+      },
+      {
+        id: 6,
+        date: "22 Fri",
+        amountThisWeek: 250000,
+        amountLastWeek: 10000,
+      },
+      {
+        id: 7,
+        date: "23 Sat",
+        amountThisWeek: 50000,
+        amountLastWeek: 10000,
+      },
+    ];
+    
